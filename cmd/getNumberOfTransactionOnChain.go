86de9b6ac3dd6ba3a417e78af146d14d72a3cf64
Tan <tan.personal.mail@gmail.com>
2023-12-18 14:46:26 +0700

    Adding getNumberOfTransactionOnBlockchain CLI

--- cmd/getNumberOfTransactionOnChain.go
+++ cmd/getNumberOfTransactionOnChain.go
@@ -1,12 +1,12 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
 
+	"github.com/gavotte25/blockchain_lab1/server"
 	"github.com/spf13/cobra"
 )
 
@@ -21,13 +21,27 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("getNumberOfTransactionOnChain called")
+		blockName, _ := cmd.Flags().GetString("name")
+		// Concate to create a json file
+		blockNameJSON := blockName + ".json"
+		// Check the existence of a blockchain with blockName
+		isExist, _ := server.IsFileInDatabasePath(blockNameJSON)
+		if !isExist {
+			fmt.Printf("Blockchain with name %s does not exist. Use \"createchain\" before adding a block\n", blockName)
+		} else {
+			// If exists, read all the content of blockchain
+			var err error
+			bc, err := server.ReadJSONFromFile(blockNameJSON)
+			if err == nil {
+				fmt.Printf("Number of Transactions on Chain: %d\n", bc.GetNumberOfTransactionsOnChain()) // Print number of transaction
+			}
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(getNumberOfTransactionOnChainCmd)
-
+	getNumberOfTransactionOnChainCmd.Flags().StringP("name", "n", "", "The name of the blockchain")
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
